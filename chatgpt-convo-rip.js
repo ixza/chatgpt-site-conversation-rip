e9560f2d3f922a2ca6d9d8fe807228b9c56d908a
ixza <116339318+ixza@users.noreply.github.com>
2023-03-17 01:48:07 +0800

    Add files via upload

---
+++ chatgpt-convo-rip.js
@@ -0,0 +1,27 @@
+var conversation_body = document.getElementsByClassName("flex flex-col items-center text-sm")
+var title = document.getElementsByClassName("flex py-3 px-3 items-center gap-3 relative rounded-md cursor-pointer break-all pr-14 bg-gray-800 hover:bg-gray-800 group")[0].childNodes[1].innerText
+var messages = conversation_body[0].childNodes
+var user_name = "USER"
+var ai_name = "OPENAI"
+var output = Array()
+messages.forEach(function(elem, i){
+  if (elem.classList.contains('group')){
+		txt = messages[i].lastElementChild.innerText.replace(/\n\s*\n/g, '\n')
+		if (i % 2 == 0){
+			var json = {user_name:txt}
+		}
+		else{
+			var json = {ai_name:txt}
+		}
+		json = JSON.stringify(json)
+		output.push(json)
+  }
+})
+output = "["+output.join(',\r\n')+"]";
+var fakeelem = document.createElement("a");
+var file = new Blob([output],
+                { type: "application/json" });
+fakeelem.href = URL.createObjectURL(file);
+fakeelem.download = title+'.json';
+fakeelem.click();
+URL.revokeObjectURL(fakelem.href);
